efdac7039074863ef0788bdb507fd5c09f72dc13
nSatyabrata <nsatyabrata16@gmail.com>
2023-03-03 23:06:41 +0530

    added functionality for 5 calls per min

--- app.py
+++ app.py
@@ -15,17 +15,37 @@ API_KEY = os.environ["API_KEY"]
 
 topics = ["blockchain","earnings","ipo","mergers_and_acquisitions","financial_markets","economy_fiscal","economy_monetary","economy_macro","energy_transportation","finance","life_sciences","manufacturing","real_estate","retail_wholesale","technology"]
 
-# API_URL = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics={topics[1]}&apikey={API_KEY}&time_from={yesterday}&time_to={now}&sort=latest"
-start = time.perf_counter()
+data = {}
+
+s = time.perf_counter()
+time_elapsed = 0
 # print(API_URL)
-for topic in topics:
-    s = time.perf_counter()
+for i,topic in enumerate(topics):
+    # 5 calls within a min
+    if i!= 0 and i%5 == 0:
+        time.sleep(60 - time_elapsed)
+        print(f"Pausing for {60 - time_elapsed}")
+        time_elapsed = 0
+
+    start = time.perf_counter()
     url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics={topic}&apikey={API_KEY}&time_from={yesterday}&time_to={now}&sort=latest"
-    res = requests.get(url).json()
-    if len(res) == 1:
-        print(res)
-    # Due to api call limit 5 request per 1 minute :
-    # if total api call is greater than 5 and total time taken is less than 60 second
-    # then wait for 1 min to complete then continue
-    print(f"{topic}: {time.perf_counter() - s}")
-print("Total: ",time.perf_counter() - start)
+    response = requests.get(url).json()
+    
+    if len(response) == 1:
+        print(response)
+    else:
+        if int(response['items']) > 20:
+            data[topic] = response['feed'][:20]
+        else:
+            data[topic] = response['feed']
+        
+    end = time.perf_counter()
+    diff = end - start
+    time_elapsed += diff
+    print(f"{topic,i}: {diff}")
+print(len(data))
+
+with open('market_news.json', "w") as f:
+    json.dump(data, f, indent=4)
+
+print("Total: ",time.perf_counter() - s)
