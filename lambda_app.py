da17632b484b88f11ba524505941b96530dd3c3b
nSatyabrata <nsatyabrata16@gmail.com>
2023-03-04 22:40:12 +0530

    Added logging and upload to s3 functionality

--- lambda_app.py
+++ lambda_app.py
@@ -1,9 +1,6 @@
-import requests, json, os, time, logging, boto3, sys
-from dotenv import load_dotenv
+import requests, json, os, time, logging, boto3
 from datetime import datetime, timedelta
 from typing import Optional, Dict, List
-
-load_dotenv()
 
 API_KEY = os.environ["API_KEY"]
 
@@ -12,27 +9,12 @@ YESTERDAY = NOW - timedelta(days=1)
 
 TOPICS = ["blockchain","earnings","ipo","mergers_and_acquisitions","financial_markets","economy_fiscal","economy_monetary","economy_macro","energy_transportation","finance","life_sciences","manufacturing","real_estate","retail_wholesale","technology"]
 
-logger = logging.getLogger("News-Logger")
-logger.setLevel(logging.INFO)
-
-if len(logging.getLogger().handlers) > 0:
-    logging.getLogger().setLevel(logging.INFO)
-    logging.getLogger().handlers[0].setFormatter(
-        logging.Formatter('%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s')
-    )
-else:
-    s_handler = logging.StreamHandler(sys.stdout)
-    s_handler.setLevel(logging.INFO)
-    s_handler.setFormatter(
-        logging.Formatter('%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s')
-    )
-    logging.getLogger().addHandler(s_handler)
-
 
 class S3UploadError(Exception):
     def __init__(self, message) -> None:
         self.message = message
         super().__init__(self.message)
+
 
 def get_url_response(url: str) -> Optional[Dict]:
     try:
@@ -52,6 +34,7 @@ def extract_news_data_of_topics(topics: List, time_from: str, time_to: str) -> s
         # 5 calls within a min
         if i!= 0 and i%5 == 0:
             time.sleep(60 - time_elapsed)
+            print(f"Pausing for {60 - time_elapsed}")
             time_elapsed = 0
 
         start = time.perf_counter()
@@ -67,6 +50,7 @@ def extract_news_data_of_topics(topics: List, time_from: str, time_to: str) -> s
         end = time.perf_counter()
         diff = end - start
         time_elapsed += diff
+        print(f"{topic,i}: {diff}")
 
     data = json.dumps(data, indent=3)
     return data
@@ -81,6 +65,7 @@ def upload_data_s3(data: str , bucket:str, key:str):
 
 if __name__ == "__main__":
 
+
     # Date format for url: YYYYMMDDTHHMM 
     now = NOW.strftime("%Y%m%dT%H%M")
     yesterday = YESTERDAY.strftime("%Y%m%dT%H%M")
@@ -90,9 +75,11 @@ if __name__ == "__main__":
 
         today = NOW.date().strftime('%d-%m-%y')
         key = 'news/' + today + '.json'
-        bucket = 'economydataprojec'
+        bucket = 'economydataproject'
 
         upload_data_s3(data, bucket, key)
-        logger.info("Data extraction successful")
+
     except Exception as err:
-        logger.error(f"{type(err)}, {err}")
+        logging.error(f"{type(err)}, {err}")
+
+    
